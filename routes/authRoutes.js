ac8fcfc35df599fb91315bbbe0ab2278f29a83a7
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-05-31 00:09:25 +0530

    abc

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,4 @@ const { sendOTP, verifyOTP } = require('../controllers/authController');
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTP);
 router.post("/signup", signup);
-
 module.exports = router;
