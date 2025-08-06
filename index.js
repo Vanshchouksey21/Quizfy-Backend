59a463ba7412f06a53526c3a979e8e09559bf0e9
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-08-07 00:04:34 +0530

    done

--- index.js
+++ index.js
@@ -12,15 +12,9 @@ app.use(cors());
 app.use(bodyparser.json());
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => {
-  console.error("❌ MongoDB Connection Failed:", err.message);
-  process.exit(1);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Failed:", err));
 
 
 
